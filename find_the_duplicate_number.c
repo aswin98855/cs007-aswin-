8c61c82b85a3a4e27d8021f7ad17b2df6984d0b3
aswin98855 <116991167+aswin98855@users.noreply.github.com>
2022-12-21 23:28:02 +0530

    add code file

---
+++ find_the_duplicate_number.c
@@ -0,0 +1,14 @@
+int findDuplicate(int* nums, int numsSize){
+    int slow=0;
+    int fast=0;
+    do {
+        slow = nums[slow];
+        fast = nums[nums[fast]];
+    } while(slow!=fast);
+    fast = 0;
+    do {
+        slow = nums[slow];
+        fast = nums[fast];
+    } while(slow!=fast);
+    return slow;
+}
